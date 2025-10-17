211e0a2c712aedc1ca0410815bf3bcc7f00f1b32
wangyuanjie035-creator <wangyuanjie035@gmail.com>
2025-10-17 12:01:57 +0800

    Update complete-draft-order.js

--- api/complete-draft-order.js
+++ api/complete-draft-order.js
@@ -1,8 +1,19 @@
-const { setCorsHeaders } = require('./cors-config');
+// 直接设置CORS头
+function setCorsHeaders(res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://sain-pdc-test.myshopify.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+}
 
 module.exports = async (req, res) => {
   // 设置CORS头
   setCorsHeaders(res);
+
+  // 处理OPTIONS预检请求
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
   // 只允许POST请求
   if (req.method !== 'POST') {
